5566dd4aea1c514f825d89a069308a1d2b63b376
linzhiji <linzhiji@ishehui.com>
2024-08-22 16:00:31 +0800

    fix bug

--- test/test-image-enhance.py
+++ test/test-image-enhance.py
@@ -13,23 +13,34 @@ dstImage = f"{OUTPUT_DIR}{processId}.jpg"
 
 frameEnchanceModel = "frame_enhancer"
 srcImage = "resource/851724224941.jpg"
-commands = [
-    sys.executable,
-    "run.py",
-    "--frame-processors",
-    "frame_enhancer",
-    "--frame-enhancer-model",
-    "real_esrgan_x4",
-    "-t",
-    f"{srcImage}",
-    "-o",
-    f"{dstImage}",
-    "--headless",
-]
+# commands = [
+#     sys.executable,
+#     "run.py",
+#     "--frame-processors",
+#     "frame_enhancer",
+#     "--frame-enhancer-model",
+#     "real_esrgan_x4",
+#     "-t",
+#     f"{srcImage}",
+#     "-o",
+#     f"{dstImage}",
+#     "--headless",
+# ]
+# run = subprocess.run(commands, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# if run.returncode == 0:
 
-run = subprocess.run(commands, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+cmd =f"sys.executable run.py
+--frame-processors frame_enhancer \
+--frame-enhancer-model real_esrgan_x4 \
+--target {srcImage} \
+--output {dstImage} \
+--headless"
+print(cmd)
 
-if run.returncode == 0:
+result = subprocess.check_output(cmd, shell=True)
+
+
+if result.returncode == 0:
     api_logger.info(
         f"上传合成后的图片到OSS，OUTPUT_PATH: {dstImage}, processId: {processId}"
     )
