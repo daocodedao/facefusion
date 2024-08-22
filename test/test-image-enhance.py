d2b8eac6513ee8378bfe958ff70883be6a796f0e
linzhiji <linzhiji@ishehui.com>
2024-08-22 11:45:30 +0800

    fix bug

--- test/test-image-enhance.py
+++ test/test-image-enhance.py
@@ -2,6 +2,7 @@ import time,datetime,json,os
 import subprocess
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from utils.logger_settings import api_logger
 
 
 processId = int(time.time())
@@ -21,6 +22,7 @@ commands = [ sys.executable,
 run = subprocess.run(commands, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 if run.returncode == 0:
+    api_logger.info(f"上传合成后的图片到OSS，OUTPUT_PATH: {dstImage}, processId: {processId}")
     from task import taskBase
     uploadResult = taskBase.uploadFileToTos(
         dstImage, 
