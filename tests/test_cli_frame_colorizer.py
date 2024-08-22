78cd6c2edd88155401513518c514caab1436ba0f
linzhiji <linzhiji@ishehui.com>
2024-08-22 11:14:12 +0800

    fix bug

--- tests/test_cli_frame_colorizer.py
+++ tests/test_cli_frame_colorizer.py
@@ -2,6 +2,10 @@ import subprocess
 import sys
 import pytest
 
+# import 路径修改
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from facefusion.download import conditional_download
 
 
